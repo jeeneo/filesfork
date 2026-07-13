zhanghai/MaterialFiles

Feat: Implement SSH Host Key verification

app/src/main/java/me/zhanghai/android/files/storage/SftpHostKeyChangedDialogActivity.kt
@@ -1,0 +1,44 @@
+/*
+ * Copyright (c) 2026 Material Files contributors
+ * SPDX‑License‑Identifier: GPL-3.0-or-later
+ */
+
+package me.zhanghai.android.files.storage
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.commit
+import me.zhanghai.android.files.app.AppActivity
+import me.zhanghai.android.files.util.args
+import me.zhanghai.android.files.util.putArgs
+
+class SftpHostKeyChangedDialogActivity : AppActivity() {
+    private val args by args<SftpHostKeyChangedDialogFragment.Args>()
+
+    private lateinit var fragment: SftpHostKeyChangedDialogFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Calls ensureSubDecor().
+        findViewById<View>(android.R.id.content)
+        if (savedInstanceState == null) {
+            fragment = SftpHostKeyChangedDialogFragment().putArgs(args)
+            supportFragmentManager.commit {
+                add(fragment, SftpHostKeyChangedDialogFragment::class.java.name)
+            }
+        } else {
+            fragment = supportFragmentManager.findFragmentByTag(
+                SftpHostKeyChangedDialogFragment::class.java.name
+            ) as SftpHostKeyChangedDialogFragment
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (isFinishing) {
+            fragment.onFinish()
+        }
+    }
+}

app/src/main/java/me/zhanghai/android/files/storage/SftpHostKeyChangedDialogFragment.kt
@@ -1,0 +1,109 @@
+/*
+ * Copyright (c) 2026 Material Files contributors
+ * SPDX‑License‑Identifier: GPL-3.0-or-later
+ */
+
+package me.zhanghai.android.files.storage
+
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.Parcel
+import androidx.appcompat.app.AppCompatDialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.parcelize.Parceler
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.WriteWith
+import me.zhanghai.android.files.R
+import me.zhanghai.android.files.util.ParcelableArgs
+import me.zhanghai.android.files.util.RemoteCallback
+import me.zhanghai.android.files.util.args
+import me.zhanghai.android.files.util.finish
+import me.zhanghai.android.files.util.getArgs
+import me.zhanghai.android.files.util.putArgs
+import me.zhanghai.android.files.util.readParcelable
+
+class SftpHostKeyChangedDialogFragment : AppCompatDialogFragment() {
+    private val args by args<Args>()
+
+    private var isListenerNotified = false
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        MaterialAlertDialogBuilder(requireContext(), theme)
+            .setTitle(R.string.storage_sftp_host_key_changed_title)
+            .setMessage(
+                getMessage(args.authority, args.oldHostKey, args.newHostKey, requireContext())
+            )
+            .setPositiveButton(R.string.storage_sftp_host_key_changed_trust) { _, _ ->
+                notifyListenerOnce(true)
+                finish()
+            }
+            .setNegativeButton(android.R.string.cancel) { _, _ ->
+                notifyListenerOnce(false)
+                finish()
+            }
+            .create()
+            .apply { setCanceledOnTouchOutside(false) }
+
+    override fun onCancel(dialog: DialogInterface) {
+        super.onCancel(dialog)
+
+        notifyListenerOnce(false)
+        finish()
+    }
+
+    fun onFinish() {
+        notifyListenerOnce(false)
+    }
+
+    private fun notifyListenerOnce(trust: Boolean) {
+        if (isListenerNotified) {
+            return
+        }
+        args.listener(trust)
+        isListenerNotified = true
+    }
+
+    companion object {
+        fun getMessage(authority: String, context: Context): String =
+            context.getString(R.string.storage_sftp_host_key_changed_message_format, authority)
+
+        private fun getMessage(
+            authority: String,
+            oldHostKey: String,
+            newHostKey: String,
+            context: Context
+        ): String = context.getString(
+            R.string.storage_sftp_host_key_changed_message_with_keys_format,
+            authority,
+            oldHostKey,
+            newHostKey
+        )
+    }
+
+    @Parcelize
+    class Args(
+        val authority: String,
+        val oldHostKey: String,
+        val newHostKey: String,
+        val listener: @WriteWith<ListenerParceler>() (Boolean) -> Unit
+    ) : ParcelableArgs {
+        object ListenerParceler : Parceler<(Boolean) -> Unit> {
+            override fun create(parcel: Parcel): (Boolean) -> Unit =
+                parcel.readParcelable<RemoteCallback>()!!.let {
+                    { trust -> it.sendResult(Bundle().putArgs(ListenerArgs(trust))) }
+                }
+
+            override fun ((Boolean) -> Unit).write(parcel: Parcel, flags: Int) {
+                parcel.writeParcelable(RemoteCallback {
+                    val args = it.getArgs<ListenerArgs>()
+                    this(args.trust)
+                }, flags)
+            }
+
+            @Parcelize
+            private class ListenerArgs(val trust: Boolean) : ParcelableArgs
+        }
+    }
+}

app/src/main/java/me/zhanghai/android/files/storage/SftpServerAuthenticator.kt
@@ -5,14 +5,27 @@
 
 package me.zhanghai.android.files.storage
 
+import androidx.core.content.edit
+import kotlinx.coroutines.runBlocking
+import me.zhanghai.android.files.R
+import me.zhanghai.android.files.app.BackgroundActivityStarter
+import me.zhanghai.android.files.app.application
+import me.zhanghai.android.files.app.defaultSharedPreferences
 import me.zhanghai.android.files.provider.sftp.client.Authentication
 import me.zhanghai.android.files.provider.sftp.client.Authenticator
 import me.zhanghai.android.files.provider.sftp.client.Authority
 import me.zhanghai.android.files.settings.Settings
+import me.zhanghai.android.files.util.createIntent
+import me.zhanghai.android.files.util.putArgs
 import me.zhanghai.android.files.util.valueCompat
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 object SftpServerAuthenticator : Authenticator {
+    private const val HOST_KEY_PREFIX = "sftp_host_key_"
+
     private val transientServers = mutableSetOf<SftpServer>()
+    private val hostKeyLock = Any()
 
     override fun getAuthentication(authority: Authority): Authentication? {
         val server = synchronized(transientServers) {
@@ -23,6 +36,43 @@ object SftpServerAuthenticator : Authenticator {
         return server?.authentication
     }
 
+    override fun getHostKey(authority: Authority): String? = synchronized(hostKeyLock) {
+        defaultSharedPreferences.getString(authority.toHostKeyPreferenceKey(), null)
+    }
+
+    override fun putHostKey(authority: Authority, hostKey: String) {
+        synchronized(hostKeyLock) {
+            defaultSharedPreferences.edit { putString(authority.toHostKeyPreferenceKey(), hostKey) }
+        }
+    }
+
+    override fun confirmChangedHostKey(
+        authority: Authority,
+        oldHostKey: String,
+        newHostKey: String
+    ): Boolean = runBlocking {
+        suspendCoroutine { continuation ->
+            val authorityString = authority.toString()
+            val onTrustDecision: (Boolean) -> Unit = { trust ->
+                if (trust) {
+                    putHostKey(authority, newHostKey)
+                }
+                continuation.resume(trust)
+            }
+
+            BackgroundActivityStarter.startActivity(
+                SftpHostKeyChangedDialogActivity::class.createIntent().putArgs(
+                    SftpHostKeyChangedDialogFragment.Args(
+                        authorityString, oldHostKey, newHostKey, onTrustDecision
+                    )
+                ),
+                application.getString(R.string.storage_sftp_host_key_changed_title),
+                SftpHostKeyChangedDialogFragment.getMessage(authorityString, application),
+                application
+            )
+        }
+    }
+
     fun addTransientServer(server: SftpServer) {
         synchronized(transientServers) { transientServers += server }
     }
@@ -30,4 +80,6 @@ object SftpServerAuthenticator : Authenticator {
     fun removeTransientServer(server: SftpServer) {
         synchronized(transientServers) { transientServers -= server }
     }
+
+    private fun Authority.toHostKeyPreferenceKey(): String = "$HOST_KEY_PREFIX$this"
 }

jeeneo/filesfork

app/src/main/java/me/zhanghai/android/files/storage/SftpHostKeyChangedDialogActivity.kt
@@ -1,0 +1,44 @@
+/*
+ * Copyright (c) 2026 Material Files contributors
+ * SPDX‑License‑Identifier: GPL-3.0-or-later
+ */
+
+package me.zhanghai.android.files.storage
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.commit
+import me.zhanghai.android.filesfork.app.AppActivity
+import me.zhanghai.android.filesfork.util.args
+import me.zhanghai.android.filesfork.util.putArgs
+
+class SftpHostKeyChangedDialogActivity : AppActivity() {
+    private val args by args<SftpHostKeyChangedDialogFragment.Args>()
+
+    private lateinit var fragment: SftpHostKeyChangedDialogFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Calls ensureSubDecor().
+        findViewById<View>(android.R.id.content)
+        if (savedInstanceState == null) {
+            fragment = SftpHostKeyChangedDialogFragment().putArgs(args)
+            supportFragmentManager.commit {
+                add(fragment, SftpHostKeyChangedDialogFragment::class.java.name)
+            }
+        } else {
+            fragment = supportFragmentManager.findFragmentByTag(
+                SftpHostKeyChangedDialogFragment::class.java.name
+            ) as SftpHostKeyChangedDialogFragment
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (isFinishing) {
+            fragment.onFinish()
+        }
+    }
+}

app/src/main/java/me/zhanghai/android/files/storage/SftpHostKeyChangedDialogFragment.kt
@@ -1,0 +1,109 @@
+/*
+ * Copyright (c) 2026 Material Files contributors
+ * SPDX‑License‑Identifier: GPL-3.0-or-later
+ */
+
+package me.zhanghai.android.files.storage
+
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.Parcel
+import androidx.appcompat.app.AppCompatDialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.parcelize.Parceler
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.WriteWith
+import me.zhanghai.android.filesfork.R
+import me.zhanghai.android.filesfork.util.ParcelableArgs
+import me.zhanghai.android.filesfork.util.RemoteCallback
+import me.zhanghai.android.filesfork.util.args
+import me.zhanghai.android.filesfork.util.finish
+import me.zhanghai.android.filesfork.util.getArgs
+import me.zhanghai.android.filesfork.util.putArgs
+import me.zhanghai.android.filesfork.util.readParcelable
+
+class SftpHostKeyChangedDialogFragment : AppCompatDialogFragment() {
+    private val args by args<Args>()
+
+    private var isListenerNotified = false
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        MaterialAlertDialogBuilder(requireContext(), theme)
+            .setTitle(R.string.storage_sftp_host_key_changed_title)
+            .setMessage(
+                getMessage(args.authority, args.oldHostKey, args.newHostKey, requireContext())
+            )
+            .setPositiveButton(R.string.storage_sftp_host_key_changed_trust) { _, _ ->
+                notifyListenerOnce(true)
+                finish()
+            }
+            .setNegativeButton(android.R.string.cancel) { _, _ ->
+                notifyListenerOnce(false)
+                finish()
+            }
+            .create()
+            .apply { setCanceledOnTouchOutside(false) }
+
+    override fun onCancel(dialog: DialogInterface) {
+        super.onCancel(dialog)
+
+        notifyListenerOnce(false)
+        finish()
+    }
+
+    fun onFinish() {
+        notifyListenerOnce(false)
+    }
+
+    private fun notifyListenerOnce(trust: Boolean) {
+        if (isListenerNotified) {
+            return
+        }
+        args.listener(trust)
+        isListenerNotified = true
+    }
+
+    companion object {
+        fun getMessage(authority: String, context: Context): String =
+            context.getString(R.string.storage_sftp_host_key_changed_message_format, authority)
+
+        private fun getMessage(
+            authority: String,
+            oldHostKey: String,
+            newHostKey: String,
+            context: Context
+        ): String = context.getString(
+            R.string.storage_sftp_host_key_changed_message_with_keys_format,
+            authority,
+            oldHostKey,
+            newHostKey
+        )
+    }
+
+    @Parcelize
+    class Args(
+        val authority: String,
+        val oldHostKey: String,
+        val newHostKey: String,
+        val listener: @WriteWith<ListenerParceler>() (Boolean) -> Unit
+    ) : ParcelableArgs {
+        object ListenerParceler : Parceler<(Boolean) -> Unit> {
+            override fun create(parcel: Parcel): (Boolean) -> Unit =
+                parcel.readParcelable<RemoteCallback>()!!.let {
+                    { trust -> it.sendResult(Bundle().putArgs(ListenerArgs(trust))) }
+                }
+
+            override fun ((Boolean) -> Unit).write(parcel: Parcel, flags: Int) {
+                parcel.writeParcelable(RemoteCallback {
+                    val args = it.getArgs<ListenerArgs>()
+                    this(args.trust)
+                }, flags)
+            }
+
+            @Parcelize
+            private class ListenerArgs(val trust: Boolean) : ParcelableArgs
+        }
+    }
+}

app/src/main/java/me/zhanghai/android/files/storage/SftpServerAuthenticator.kt
@@ -5,14 +5,29 @@
 
 package me.zhanghai.android.filesfork.storage
 
+import androidx.core.content.edit
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import me.zhanghai.android.files.storage.SftpHostKeyChangedDialogActivity
+import me.zhanghai.android.files.storage.SftpHostKeyChangedDialogFragment
+import me.zhanghai.android.filesfork.R
+import me.zhanghai.android.filesfork.app.BackgroundActivityStarter
+import me.zhanghai.android.filesfork.app.application
+import me.zhanghai.android.filesfork.app.defaultSharedPreferences
 import me.zhanghai.android.filesfork.provider.sftp.client.Authentication
 import me.zhanghai.android.filesfork.provider.sftp.client.Authenticator
 import me.zhanghai.android.filesfork.provider.sftp.client.Authority
 import me.zhanghai.android.filesfork.settings.Settings
+import me.zhanghai.android.filesfork.util.createIntent
+import me.zhanghai.android.filesfork.util.putArgs
 import me.zhanghai.android.filesfork.util.valueCompat
+import kotlin.coroutines.resume
 
 object SftpServerAuthenticator : Authenticator {
+    private const val HOST_KEY_PREFIX = "sftp_host_key_"
+
     private val transientServers = mutableSetOf<SftpServer>()
+    private val hostKeyLock = Any()
 
     override fun getAuthentication(authority: Authority): Authentication? {
         val server = synchronized(transientServers) {
@@ -23,6 +38,43 @@ object SftpServerAuthenticator : Authenticator {
         return server?.authentication
     }
 
+    override fun getHostKey(authority: Authority): String? = synchronized(hostKeyLock) {
+        defaultSharedPreferences.getString(authority.toHostKeyPreferenceKey(), null)
+    }
+
+    override fun putHostKey(authority: Authority, hostKey: String) {
+        synchronized(hostKeyLock) {
+            defaultSharedPreferences.edit { putString(authority.toHostKeyPreferenceKey(), hostKey) }
+        }
+    }
+
+    override fun confirmChangedHostKey(
+        authority: Authority,
+        oldHostKey: String,
+        newHostKey: String
+    ): Boolean = runBlocking {
+        suspendCancellableCoroutine { continuation ->
+            val authorityString = authority.toString()
+            val onTrustDecision: (Boolean) -> Unit = { trust ->
+                if (trust) {
+                    putHostKey(authority, newHostKey)
+                }
+                continuation.resume(trust)
+            }
+
+            BackgroundActivityStarter.startActivity(
+                SftpHostKeyChangedDialogActivity::class.createIntent().putArgs(
+                    SftpHostKeyChangedDialogFragment.Args(
+                        authorityString, oldHostKey, newHostKey, onTrustDecision
+                    )
+                ),
+                application.getString(R.string.storage_sftp_host_key_changed_title),
+                SftpHostKeyChangedDialogFragment.getMessage(authorityString, application),
+                application
+            )
+        }
+    }
+
     fun addTransientServer(server: SftpServer) {
         synchronized(transientServers) { transientServers += server }
     }
@@ -30,4 +82,6 @@ object SftpServerAuthenticator : Authenticator {
     fun removeTransientServer(server: SftpServer) {
         synchronized(transientServers) { transientServers -= server }
     }
+
+    private fun Authority.toHostKeyPreferenceKey(): String = "$HOST_KEY_PREFIX$this"
 }